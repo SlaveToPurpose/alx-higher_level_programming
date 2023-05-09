5426717297cc65bed6248f2ff8fb6960588ed319
SlaveToPurpose <keziahleroux@gmail.com>
2023-05-09 12:56:52 -0700

    fixed requirement issues

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for digit in range(0, 99):
-    print(F"{digit:d} = 0x{digit:x}")
+    print("{:d} = 0x{:x}".format(digit, digit))
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 for digit in range(0, 100):
-    end = "\n" if digit == 99 else ", "
-    print(F"{digit:02d}", end=end)
+    print("{:02d}".format(digit), end="\n" if digit == 99 else ", ")
