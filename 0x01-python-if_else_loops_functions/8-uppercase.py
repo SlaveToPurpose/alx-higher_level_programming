ce736b6f6236fd9f2e660fad6fff42ddcbf42120
SlaveToPurpose <122865752+SlaveToPurpose@users.noreply.github.com>
2023-05-09 22:54:57 +0000

    uppercase print

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(string):
+    string_1 = ""
+    for i in string:
+        if ord(i) > 96 and ord(i) < 123:
+             string_1 += "{:c}".format(ord(i) - 32)
+        else:
+            string_1 += i
+
+    print(string_1)
