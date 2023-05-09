60400f1ee4317aab94faf4ae236de6145a9e9d8d
SlaveToPurpose <122865752+SlaveToPurpose@users.noreply.github.com>
2023-05-09 03:57:43 +0000

    format adjustments to code

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(str+str+str)
-print(str[0:9])
+print(str * 3)
+print(F"{str:.9s}")
--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-str1_2 = str1 + str2
-print(F"Welcome to {str1_2}!")
+str1 = str1 + str2 + "!"
+print(F"Welcome to {str1:s}")
