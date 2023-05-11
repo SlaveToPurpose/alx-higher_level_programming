ce5ec4c4cfb346f0b6ecac885bb002226901eb62
SlaveToPurpose <keziahleroux@gmail.com>
2023-05-11 15:55:23 -0700

    improve efficiency of type conversion

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,18 +4,19 @@ from sys import argv
 
 if __name__ == "__main__":
     if len(argv) == 4:
+        arg1 = int(argv[1])
+        arg3 = int(argv[3])
         if argv[2] == '+':
-            print("{:d} + {:d} = {:d}".format(int(argv[1]), int(argv[3]), add(int(argv[1]), int(argv[3]))))
+            print("{:d} + {:d} = {:d}".format(arg1, arg3, add(arg1, arg3)))
         elif argv[2] == '-':
-            print("{:d} - {:d} = {:d}".format(int(argv[1]), int(argv[3]), sub(int(argv[1]), int(argv[3]))))
+            print("{:d} - {:d} = {:d}".format(arg1, arg3, sub(arg1, arg3)))
         elif argv[2] == '*':
-            print("{:d} * {:d} = {:d}".format(int(argv[1]), int(argv[3]), mul(int(argv[1]), int(argv[3]))))
+            print("{:d} * {:d} = {:d}".format(arg1, arg3, mul(arg1, arg3)))
         elif argv[2] == '/':
-            print("{:d} / {:d} = {:d}".format(int(argv[1]), int(argv[3]), div(int(argv[1]), int(argv[3]))))
+            print("{:d} / {:d} = {:d}".format(arg1, arg3, div(arg1, arg3)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
     else:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
