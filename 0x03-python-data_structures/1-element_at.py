cc6c6f2d985e9c43691e5a384f56e4504d2deb8d
SlaveToPurpose <keziahleroux@gmail.com>
2023-05-12 08:43:22 -0700

    replace the list + reverse the list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    elif idx + 1 > len(my_list):
+        return None
+    else:
+        element = my_list[idx]
+        return element
