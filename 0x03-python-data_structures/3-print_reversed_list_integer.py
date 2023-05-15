03fccdd168b0418eae039c5682c0092f57a2ed49
SlaveToPurpose <keziahleroux@gmail.com>
2023-05-15 02:15:05 -0700

    Revert "dealt with empty list issue on task 3"
    This reverts commit 119edf2061f818d2d76375b22047cfcb84f64219.

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) != 0:
-        indx = len(my_list) - 1
-        while indx >= 0:
-            print("{:d}".format(my_list[indx]))
-            indx -= 1
-    else:
-        print("")
+    indx = len(my_list) - 1
+    while indx >= 0:
+        print("{:d}".format(my_list[indx]))
+        indx -= 1
