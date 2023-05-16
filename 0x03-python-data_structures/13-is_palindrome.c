41a3215c6a2fb82ff270d6f435d9d65b924f471e
SlaveToPurpose <keziahleroux@gmail.com>
2023-05-15 17:27:00 -0700

    palindome files

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if singly linked list is palindrome
+ * @head: pointer to head of list
+ *
+ * Return: 0 if not palindrome, else 1
+ */
+
+int is_palindrome(listint_t **head)
+{
+
