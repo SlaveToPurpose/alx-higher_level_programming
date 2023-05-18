4af3df06dd37f68bb33db8abc1daf4b4cae9e839
SlaveToPurpose <122865752+SlaveToPurpose@users.noreply.github.com>
2023-05-18 02:38:01 +0000

    using map to compute sqare of integers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) != str:
+    if not roman_string or type(roman_string) != str:
         return 0
     roman_vals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     final_int_val = 0
-
     for char in range(len(roman_string)):
         value = roman_vals[roman_string[char]]
         if char < len(roman_string) - 1 and value < roman_vals[roman_string[char + 1]]:
