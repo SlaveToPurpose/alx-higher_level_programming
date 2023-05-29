1daf012977d0e261bfef500a97b5414cc290d2b9
SlaveToPurpose <keziahleroux@gmail.com>
2023-05-29 13:36:23 -0700

    print integer

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    pop = 0
+    if x <= 0:
+        return pop
+    else:
+        for element in range(x):
+            try:
+                print("{:d}".format(my_list[element]), end='')
+                pop += 1
+            except (ValueError, TypeError):
+                pass
+    print('')
+    return pop
