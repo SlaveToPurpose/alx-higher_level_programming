0a534ebcc837b63dd39e6133115c427ea2981190
SlaveToPurpose <122865752+SlaveToPurpose@users.noreply.github.com>
2023-05-30 19:42:46 +0000

    task 2 documentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,7 +3,13 @@
 
 
 class Square:
+    """square."""
     def __init__(self, size=0):
+         """initialise new square.
+
+        Args:
+            size(int): size of new square.
+        """
         if size < 0:
             raise ValueError("size must be >= 0")
         elif not isinstance(size, int):
