6eb46fbb46035c4e91e8fc0b1b85fcbc851e57ce
SlaveToPurpose <122865752+SlaveToPurpose@users.noreply.github.com>
2023-06-02 12:06:45 +0000

    divide matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -18,4 +18,24 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
+    if len(matrix) is 0:
+        raise TypeError("matrix must be a matrix (list of lists)"
+                        " of integers/floats")
         
+    for rows in matrix:
+        for columns in rows:
+            if type(column) is not int or type(column) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        if len(rows) == len(matrix[0]):
+            pass
+        else:
+            raise TypeError("Each row of the matrix must have the same size")
+        if type(rows) is list:
+            pass
+        else:
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+     if type(matrix) is not list:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    
