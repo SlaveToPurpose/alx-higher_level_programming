05d7d41b71a42126c386442f2fd9f1e0e9f5485e
SlaveToPurpose <122865752+SlaveToPurpose@users.noreply.github.com>
2023-06-02 18:54:55 +0000

    fiinal python file for matrix divide

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -12,30 +12,33 @@ def matrix_divided(matrix, div):
     Raise ZeroDivisionError
     - if div = 0
     """
-    if div is int or div is float:
+    if type(div) is int or type(div) is float:
         pass
     else:
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    if len(matrix) is 0:
+
+    if type(matrix) is not list:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    if len(matrix) == 0:
         raise TypeError("matrix must be a matrix (list of lists)"
                         " of integers/floats")
         
-    for rows in matrix:
-        for columns in rows:
-            if type(column) is not int or type(column) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        if len(rows) == len(matrix[0]):
-            pass
-        else:
+    for row in matrix:
+        if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
-        if type(rows) is list:
-            pass
-        else:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        for element in row:
+            if type(element) is not int and type(element) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                
+    divided_matrix = []
+    for row in matrix:
+        divided_row = []
+        for element in row:
+            divided_row.append(round(element / div, 2))
+        divided_matrix.append(divided_row)
 
-     if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
-    
+    return divided_matrix
