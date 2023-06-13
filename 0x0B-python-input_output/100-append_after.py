e25fd6b2823fc81ec31172596ac04b96631ce29a
SlaveToPurpose <keziahleroux@gmail.com>
2023-06-13 22:36:46 +0200

    Ssearch and apend

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""search & update"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """inserts a line of text after each line containing a specific string
+    """
+    data = ""
+
+    with open(filename, encoding="utf-8") as file:
+        for line in file:
+            data.append(line)
+            if search_string in line:
+                data += new_string
+
+    with open(filename, "w", encoding="utf-8") as file2:
+        file2.write(data)
