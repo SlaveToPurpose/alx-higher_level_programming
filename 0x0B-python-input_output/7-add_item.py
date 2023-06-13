96bd160180eeb6e33d18b59cb5862cfd4bdd3bfd
SlaveToPurpose <keziahleroux@gmail.com>
2023-06-13 06:55:09 -0700

    Revert "load, add, save"
    This reverts commit e08d030612505e4a5b1dd32852fafc1653ef048b.

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,14 +5,17 @@
 import sys
 
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    returnList = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    returnList = []
+    try:
+        returnList = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        returnList = []
 
-data.extend(sys.argv[1:])
+    if (len(sys.argv)) > 1:
+        for arg in range(1, len(sys.argv)):
+            returnList.append(sys.argv[arg])
 
-save_to_json_file(returnList, "add_item.json")
+    save_to_json_file(returnList, "add_item.json")
