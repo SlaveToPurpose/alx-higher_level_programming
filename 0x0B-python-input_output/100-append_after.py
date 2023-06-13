e9c8be66d4d1a40fc3a823dcfee28c81be86f6cc
SlaveToPurpose <keziahleroux@gmail.com>
2023-06-13 22:37:51 +0200

    Search and append

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,6 +5,7 @@
 def append_after(filename="", search_string="", new_string=""):
     """inserts a line of text after each line containing a specific string
     """
+
     data = ""
 
     with open(filename, encoding="utf-8") as file:
