ce3f080638ebe999f82c7411ce4eb4fb40bbfdba
SlaveToPurpose <122865752+SlaveToPurpose@users.noreply.github.com>
2023-06-13 11:54:28 +0000

    8.Rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''A module for working with geometry'''
+"""A module for working with geometry"""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
@@ -9,12 +9,12 @@ class Rectangle(BaseGeometry):
     """geometry obj"""
 
     def __init__(self, width, height):
-		'''Initializes Rectangle
+		"""Initializes Rectangle
 		
 		Args:
 			width (int): The width of the rectangle.
 			height (int): The height of the rectangle.
-		'''
+		"""
 
 		self.integer_validator('width', width)
 		self.integer_validator('height', height)
