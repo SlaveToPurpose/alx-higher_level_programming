ce9e56dd2458aaf81070a66d8a7af013471b0c35
SlaveToPurpose <keziahleroux@gmail.com>
2023-10-15 22:38:01 +0200

    task1 base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,26 @@
+""" “base” of all other classes.
+The goal of it is to manage id attribute in all
+your future classes and to avoid duplication
+"""
+
+class Base:
+    """Base class for managing ids"""
+
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Constructor for the Base class.
+
+        Parameters:
+        - id (int)
+        Returns:
+        None
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
