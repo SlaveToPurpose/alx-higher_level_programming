c617e17def66173d5a81517e8d91018d9433ffce
SlaveToPurpose <keziahleroux@gmail.com>
2023-10-17 00:38:03 +0200

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,102 +6,103 @@ from models.base import Base
 
 
 class Rectangle(Base):
-	"""Rectangle class child of base class"""
+    """Rectangle class child of base class"""
 
-	def __init__(self, width, height, x=0, y=0, id=None):
-		"""
-		Constructor for rectangle
-		Parameters:
-		widht(int): width of rect
-		height(int): height of rectangle
-		x(int): x coordinate of rectangle
-		y(int): y coordinate of rectangle
-		id(int): ID of rectangle 
-		
-		Returns:
-		None
-		"""
-		super().__init__(id)
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Constructor for rectangle
+        Parameters:
+        widht(int): width of rect
+        height(int): height of rectangle
+        x(int): x coordinate of rectangle
+        y(int): y coordinate of rectangle
+        id(int): ID of rectangle 
 
-		self.width = width
-		self.height = height
-		self.x = x
-		self.y =y
+        Returns:
+        None
+        """
+        super().__init__(id)
 
-	@property
-	def width(self):
-		"""Getter for width."""
-		return self.__width	
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-	@property
-	def height(self):
-		"""Getter for height."""
-		return self.__height
+    @property
+    def width(self):
+        """Getter for width."""
+        return self.__width
 
-	@property
-	def x(self):
-		"""Getter for x."""
-		return self.__x
+    @property
+    def height(self):
+        """Getter for height."""
+        return self.__height
 
-	@property
-	def y(self):
-		"""Getter for y."""
-		return self.__y
+    @property
+    def x(self):
+        """Getter for x."""
+        return self.__x
 
-	@width.setter
-	def width(self, value):
-		"""Setter for width."""
-		if type(value) is not int:
-			raise TypeError("width must be an integer")
-		if value <= 0:
-			raise ValueError("width must be > 0")
-		self.__width = value
+    @property
+    def y(self):
+        """Getter for y."""
+        return self.__y
 
-	@height.setter
-	def height(self, value):
-		"""Setter for height."""
-		if type(value) is not int:
-			raise TypeError("height must be an integer")
-		if value <= 0:
-			raise ValueError("height must be > 0")
-		self.__height = value
-	@x.setter
-	def x(self, value):
-		"""Setter for x."""
-		if type(value) is not int:
-			raise TypeError("x must be an integer")
-		if value < 0:
-			raise ValueError("x must be >= 0")
-		self.__x = value
+    @width.setter
+    def width(self, value):
+        """Setter for width."""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
-	@y.setter
-	def y(self, value):
-		"""Setter for y."""
-		if type(value) is not int:
-			raise TypeError("y must be an integer")
-		if value < 0:
-			raise ValueError("y must be >= 0")
-		self.__y = value
+    @height.setter
+    def height(self, value):
+        """Setter for height."""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
-	def area(self):
-		"""Return area value of Rectangle instance"""
-		return self.__width * self.__height
+    @x.setter
+    def x(self, value):
+        """Setter for x."""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
-	def display(self):
-		"""Print Rectangle to stdout using #
-		
-		Returns:
-		None
-		"""	
-		for i in range(self.__y):
-			print("")
-		for j in range(self.__height):
-			print(" " - self.__x + "#" * self.__width)
+    @y.setter
+    def y(self, value):
+        """Setter for y."""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
 
-	def __str__(self):
-		"""Return string representation of Rectangle
-		
-		Returns:
-		str: string representation
-		"""
-		return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+    def area(self):
+        """Return area value of Rectangle instance"""
+        return self.__width * self.__height
+
+    def display(self):
+        """Print Rectangle to stdout using #
+
+        Returns:
+        None
+        """
+        for i in range(self.__y):
+            print("")
+        for j in range(self.__height):
+            print(" " - self.__x + "#" * self.__width)
+
+    def __str__(self):
+        """Return string representation of Rectangle
+
+        Returns:
+        str: string representation
+        """
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
