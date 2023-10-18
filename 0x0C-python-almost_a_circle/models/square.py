39b53514d760ffd51496de64ba3f8a6196656565
SlaveToPurpose <keziahleroux@gmail.com>
2023-10-18 21:52:24 +0200

    args=None provision

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -66,6 +66,8 @@ class Square(Rectangle):
         None
         """
         if args:
+            if args[0] is None:
+                self.__init__(self.size, self.x, self.y)
             if len(args) >= 1:
                 self.id = args[0]
             if len(args) >= 2:
