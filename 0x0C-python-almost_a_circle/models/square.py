1792cf0412fdcf348ea48e43a39446685b8bf6fb
SlaveToPurpose <keziahleroux@gmail.com>
2023-10-18 22:11:08 +0200

    fix if conditions

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -71,7 +71,9 @@ class Square(Rectangle):
             if len(args) >= 2:
                 self.size = args[1]
             if len(args) >= 3:
-                self.y = args[2]
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y =args[3]
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
