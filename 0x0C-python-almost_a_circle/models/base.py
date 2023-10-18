31c2a8809bbcb85dfffdefd4524a3e0fbcf6f6b4
SlaveToPurpose <keziahleroux@gmail.com>
2023-10-18 22:36:52 +0200

    {} to []

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -37,6 +37,6 @@ class Base:
 
     def to_json_string(list_dictionaries):
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "{}"
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
