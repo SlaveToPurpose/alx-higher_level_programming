39877ab987dac9ad4babfecde0c36552b4957aaf
SlaveToPurpose <keziahleroux@gmail.com>
2023-11-15 13:57:37 -0800

    fix comment errors

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,3 @@
---insert new data item into table
-INSERT INTO first_table (id, name)
-VALUES (89, "Best School");
+-- insert new data item into table
+INSERT INTO first_table (id, name) VALUES (89, "Best School");
 
