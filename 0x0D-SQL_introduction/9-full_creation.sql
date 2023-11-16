32f16e7b1c2f71be8d1996aeb9caac2649f8ea49
SlaveToPurpose <keziahleroux@gmail.com>
2023-11-16 11:19:30 -0800

    format task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- create table with dull details
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table (id, name, score) VALUES (1, 'John', 90);
-INSERT INTO second_table (id, name, score) VALUES (2, 'Alice', 85);
-INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 92);
-INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
+CREATE TABLE IF NOT EXISTS 'second_table' ('id' INT, 'name' VARCHAR(256), 'score' INT);
+INSERT INTO 'second_table' ('id', 'name', 'score') VALUES (1, 'John', 90);
+INSERT INTO 'second_table' ('id', 'name', 'score') VALUES (2, 'Alice', 85);
+INSERT INTO 'second_table' '(id', 'name', 'score') VALUES (3, 'Bob', 92);
+INSERT INTO 'second_table' ('id', 'name', 'score') VALUES (4, 'George', 8);
 
