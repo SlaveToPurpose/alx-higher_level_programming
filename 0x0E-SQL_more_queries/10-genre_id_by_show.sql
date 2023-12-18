f8b430a5002d6997cb735f5d280cffbbb29ecce4
Keziah Le Roux <122865752+SlaveToPurpose@users.noreply.github.com>
2023-12-18 14:11:53 +0000

    list all shows that have 1 genre linked, correct join

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,5 @@
 -- List all shows contained in DB hbtn_0d_tvshows
-SELECT tv_shows.title, tv_shows_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NOT NULL
-order by tv_shows.title ASC, tv_show_genre_id ASC;
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
